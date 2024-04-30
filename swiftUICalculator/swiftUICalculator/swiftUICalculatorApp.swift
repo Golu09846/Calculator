eff114b74b44f113825ad636baf0987104075b5f
Golu09846 <153588269+Golu09846@users.noreply.github.com>
2024-04-30 10:14:59 +0530

    Calculator
    Here, You can create a Calculator using SwiftUI in Xcode

---
+++ swiftUICalculator/swiftUICalculator/swiftUICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUICalculatorApp.swift
+//  swiftUICalculator
+//
+//  Created by STUDENT on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+   struct swiftUICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
